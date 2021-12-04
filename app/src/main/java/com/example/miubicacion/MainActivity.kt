d4e5dad4e2bfc55a36553d063e5251ff78c1cea3
aalvarezbretana <aalvarezvbretana>
2021-12-04 23:02:53 +0100

    Proyecto completado, MiUbicación

--- app/src/main/java/com/example/miubicacion/MainActivity.kt
+++ app/src/main/java/com/example/miubicacion/MainActivity.kt
@@ -14,101 +14,169 @@ import android.content.ContentProviderOperation
 import android.content.pm.PackageManager
 import android.widget.Toast
 import android.content.DialogInterface
+import android.location.Location
 import android.os.RemoteException
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+    private lateinit var map: GoogleMap
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        enableMyLocation()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createMapFragment()
     }
 
-    private val TAG = "Contacts"
-    private fun insertDummyContact() {
-        // Two operations are needed to insert a new contact.
-        val operations = ArrayList<ContentProviderOperation>(2)
-
-        // First, set up a new raw contact.
-        var op = ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
-            .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
-            .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
-        operations.add(op.build())
-
-        // Next, set the name for the contact.
-        op = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-            .withValue(
-                ContactsContract.Data.MIMETYPE,
-                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE
-            )
-            .withValue(
-                ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
-                "__DUMMY CONTACT from runtime permissions sample"
-            )
-        operations.add(op.build())
-
-        // Apply the operations.
-        val resolver = contentResolver
-        try {
-            resolver.applyBatch(ContactsContract.AUTHORITY, operations)
-        } catch (e: RemoteException) {
-            Log.d(TAG, "Could not add a new contact: " + e.message)
-        } catch (e: OperationApplicationException) {
-            Log.d(TAG, "Could not add a new contact: " + e.message)
-        }
+    // funcion que implementa el fragment del mapa
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    private val REQUEST_CODE_ASK_PERMISSIONS = 123
-
-    private fun insertDummyContactWrapper() {
-        val hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_CONTACTS)
-        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
-            if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
-                showMessageOKCancel(
-                    "You need to allow access to Contacts"
-                ) { dialog, which ->
-                    requestPermissions(
-                        arrayOf(Manifest.permission.WRITE_CONTACTS),
-                        REQUEST_CODE_ASK_PERMISSIONS
-                    )
-                }
+    // funcion para comprobar si se ha dado o no el permiso
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+    // funcion que comprueba si el mapa se ha iniciado, si es así, comprueba los permisos con la siguiente función
+    private fun enableMyLocation() {
+        if (!::map.isInitialized) return
+        if (isPermissionsGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
                 return
             }
-            requestPermissions(
-                arrayOf(Manifest.permission.WRITE_CONTACTS),
-                REQUEST_CODE_ASK_PERMISSIONS
-            )
-            return
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
         }
-        insertDummyContact()
     }
 
-    private fun showMessageOKCancel(message: String, okListener: DialogInterface.OnClickListener) {
-        AlertDialog.Builder(this@MainActivity)
-            .setMessage(message)
-            .setPositiveButton("OK", okListener)
-            .setNegativeButton("Cancel", null)
-            .create()
-            .show()
+    // esta es la siguiente función, nombrada arriba
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
     }
-
+    // función que nos avisa que ha aceptado los permisos que le hemos pedido
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<String?>,
+        permissions: Array<out String>,
         grantResults: IntArray
     ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            REQUEST_CODE_ASK_PERMISSIONS -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission Granted
-                insertDummyContact()
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                map.isMyLocationEnabled = true
             } else {
-                // Permission Denied
-                Toast.makeText(this@MainActivity, "WRITE_CONTACTS Denied", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(
+                    this,
+                    "Para activar la localización ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-            else -> super.onRequestPermissionsResult(requestCode, permissions!!, grantResults)
+            else -> {}
         }
     }
+    // por si se cierra la aplicacion y se quitan los permisos, para que no deje de funcionar creamos la siguiente función
+    // que comprueba si el mapa está cargado, si está cargado sale del método y vuelve a comprobar si los permisos siguen activos
+    // si no lo están desactiva la localización en tiempo real
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if (!isPermissionsGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            map.isMyLocationEnabled = false
+            Toast.makeText(
+                this,
+                "Para activar la localización ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+    // función para centrar la aplicación en la localizacion, si ya estás en esa localizacion no se ejecuta el botón
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Boton pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+    // función que recibe la localizacion al pulsar en el usuario y muestra en un toast la latitud y longitud
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    }
 }
-
